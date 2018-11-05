5b51d032256ebb131744fe692747edefb595aa9e
Preston Wallace <wallace.preston@gmail.com>
2018-11-05 15:07:29 -0800

    split done, merge done

--- mergesort.js
+++ mergesort.js
@@ -7,16 +7,25 @@ function split (wholeArray){
 
 console.log(split([3,4,5,6,9,8,6]));
 
+
 function merge (sortedArray1, sortedArray2){
   let mergedArray=[];
-  while (sortedArray1.length){
-  if (sortedArray1[0] < sortedArray2[0]){
-    mergedArray.push(sortedArray1.unshift());
-  } else {
-      if(sortedArray2.length){
-      mergedArray.push(sortedArray2.unshift());
-      }
+  while (sortedArray1.length && sortedArray2.length){
+    if (sortedArray1[0] < sortedArray2[0]){
+      mergedArray.push(sortedArray1.shift());
+    } else {
+        if(sortedArray2.length){
+          mergedArray.push(sortedArray2.shift());
+        }
+    }
   }
-}
+  if(sortedArray1.length){
+    mergedArray.push(...sortedArray1)
+  } else if (sortedArray2.length){
+    mergedArray.push(...sortedArray2)
+  }
+
   return mergedArray;
 }
+
+merge([1,3,5,7],[2,4,6])
